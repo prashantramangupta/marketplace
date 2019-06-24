7b378ac6d6475067d706a9a3e0335e900af471dd
prashantramangupta <you@example.com>
2019-06-24 18:26:38 +0530

    API's to store user data, link pre-seeded wallet and deregister user

---
+++ signup_api/common/constant.py
@@ -0,0 +1,4 @@
+NETWORKS = {
+}
+SLACK_HOOK = {
+}
---
+++ signup_api/common/utils.py
@@ -0,0 +1,30 @@
+import datetime
+import decimal
+
+IGNORED_LIST = ['row_id', 'row_created', 'row_updated']
+
+
+class Utils:
+    def __init__(self):
+        pass
+
+    def clean(self, value_list):
+        for value in value_list:
+            self.clean_row(value)
+
+    def clean_row(self, row):
+        for item in IGNORED_LIST:
+            del row[item]
+
+        for key in row:
+            if isinstance(row[key], decimal.Decimal) or isinstance(row[key], datetime.datetime):
+                row[key] = str(row[key])
+            elif isinstance(row[key], bytes):
+                if row[key] == b'\x01':
+                    row[key] = 1
+                elif row[key] == b'\x00':
+                    row[key] = 0
+                else:
+                    raise Exception("Unsupported bytes object. Key " + str(key) + " value " + str(row[key]))
+
+        return row
